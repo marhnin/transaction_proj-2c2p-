d520877f5cf41aaeba27618599150238876979e5
itworlddev2021 <itworlddev2021@gmail.com>
2022-12-03 09:21:25 +0630

    create project and design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'transaction-proj';
+  file = File  ; // Variable to store file
+  ngOnInit(): void
+  {
+   
+  }
+ // On file Select
+ onChange(event:any)
+ {
+  const file = event.target.files[0];
+  console.log("File "+file.size);
+  console.log("File "+file.type);
+ }
+  // OnClick of button Upload
+  onUpload() 
+  {
+  }
 }
